ede0975b8c8eed79a9f41984abdc0fc60bd1a7e7
Jake <jakea1865@gmail.com>
2024-08-01 00:13:20 -0700

    Add getComputerChoice
    Working

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,15 @@
-console.log()
+// define function getComputerChoice
+function getComputerChoice(){
+   let rand = Math.floor(Math.random() * 101);
+if (rand >= 66) {
+    return 'Rock'
+}
+else if (rand >= 33) {
+    return 'Paper'
+}
+else {
+    return 'Scissors'
+}
+}
+console.log(getComputerChoice())
+// return rock, paper, scissors
